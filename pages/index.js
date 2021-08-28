1a50496e2c78dac007496485cd1cab8846277715
Nicholas Berlette <nick@berlette.com>
2021-08-27 23:55:58 -0700

    update index.js for preact

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 export default function Index() {
   return (
-    <h1>
-      You're running React on the Edge!
-    </h1>
+    <>
+      <h1>
+	  {"You're running Preact on the Edge!"}
+      </h1>
+      <p>
+	  Congratulations, if you happen to be reading this message it means Phlareact (Preact + Flareact) has successfully deployed via Cloudflare Workers.
+      </p>
+      <p>
+	  <strong>Huzzah!</strong> And... what now? <a href="https://preactjs.org/">PreactJS</a> and <a href="https://flareact.com">Flareact.com</a> both have some very helpful websites and strong documentation. Check out the source code and start building!
+      </p> 
+    </>
   );
 }
